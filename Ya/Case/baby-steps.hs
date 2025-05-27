2cd53278cb6de73975d56eb3d29f7880fcf7d938
Murat Kasimov <iokasimov.m@gmail.com>
2025-05-27 21:11:14 +0700

    Update baby-steps executable

--- Ya/Case/baby-steps.hs
+++ Ya/Case/baby-steps.hs
@@ -7,26 +7,11 @@ import "ya-console" Ya.Console
 import "base" GHC.Num (Integer)
 import "base" System.IO (print)
 
-main = print @(Unit `P` Unit) (Unit `hj` is `hv` Unit)
+-- main = print @(Unit `P` Unit) (Unit `hj` is `hv` Unit)
 
 -- (i `AR` a) `P` i -> into (from a o) o
 -- main = print @Unit (is `lu` Unit `hd_` is)
 
--- example = is @(List Integer) [1, 2, 3, 4, 5] `lu'yp` Cross 
+parse = on @Glyph `ho'ho` on @Digit `ho` row
 
--- main = example `yok` Cascading `ha` intro
--- main = example
- -- `yokl` Forth `ha` World `ha` print
-
--- main = is @(List Integer) [1, 2, 3]
---    `lu'yp` Align @List [4, 5, 6]
---    `yokl` Forth `ha` World `ha` print
-
--- main = is @(Nonempty List Integer) [1, 2, 3]
---  `kyo` Range `ha` is @(Nonempty List Integer)
---  `yokl` Forth `ha` World
---  `ha__'yuk` World `ha` output `ha` Glyph `ha` Symbol `ha` Punctuate `hv` by Space
---  `ha__'yokl` Forth `ha` World `ha` print
-
--- main = is @(Optional `T'I` Nonempty List Integer) `hv` Some [1, 2, 3]
---  `yokl'yokl` Try `ha` Forth `ha` World `ha` print
+main = input `yok` Retry `ha` Perhaps `ha` parse
